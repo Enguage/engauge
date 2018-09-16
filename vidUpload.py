0db1bfe56da9e4b12141594b0ccb4d0155cc0fea
Roesha Nigos <ishanigos@dhcp-18-30-67-96.dyn.mit.edu>
2018-09-15 22:36:56 -0500

    upload

--- vidUpload.py
+++ vidUpload.py
@@ -6,6 +6,7 @@ accountID = "e6dc5dac-de07-48a6-b157-91105accc78a"
 accAccessToken = ""
 videoAccessToken = ""
 
+
 #account token
 headers1 = {
     # Request headers
@@ -24,7 +25,7 @@ try:
     response = conn.getresponse()
     string = response.read().decode('utf-8')
     json_obj = json.loads(string)
-    # print(json_obj)
+    # print(json_obj\
     accAccessToken = json_obj
     #print data is the access token
     conn.close()
@@ -46,7 +47,7 @@ params = urllib.parse.urlencode({
 
 try:
     conn = http.client.HTTPSConnection('api.videoindexer.ai')
-    conn.request("GET", "/auth/westus2/Accounts/e6dc5dac-de07-48a6-b157-91105accc78a/Videos/56600908f1/AccessToken?%s" % params, "{body}", headers2)
+    conn.request("GET", "/auth/westus2/Accounts/e6dc5dac-de07-48a6-b157-91105accc78a/Videos/594cbabeab/AccessToken?%s" % params, "{body}", headers2)
     response = conn.getresponse()
     string = response.read().decode('utf-8')
     json_obj = json.loads(string)
@@ -58,7 +59,25 @@ except Exception as e:
     print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
 
+#this is the video list.
+headersForVids = {
+}
 
+params = urllib.parse.urlencode({
+    # Request parameters
+    'pageSize': '{integer}',
+    'skip': '{integer}',
+})
+
+try:
+    conn = http.client.HTTPSConnection('api.videoindexer.ai')
+    conn.request("GET", "/westus2/Accounts/e6dc5dac-de07-48a6-b157-91105accc78a/Videos?accessToken=" + accAccessToken + "&%s" % params, "{body}", headersForVids)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
 
 #step 3
@@ -83,7 +102,7 @@ def parser(data):
 
 try:
     conn = http.client.HTTPSConnection('api.videoindexer.ai')
-    conn.request("GET", "/westus2/Accounts/e6dc5dac-de07-48a6-b157-91105accc78a/Videos/56600908f1/Index?%s" % params, "{body}", headers3)
+    conn.request("GET", "/westus2/Accounts/e6dc5dac-de07-48a6-b157-91105accc78a/Videos/594cbabeab/Index?%s" % params, "{body}", headers3)
     response = conn.getresponse()
     data = response.read()
     # thing that needs to be parsed 
