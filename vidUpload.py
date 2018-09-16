7cbcbd8aa0f06dea197c941f3b032636278acbf0
Roesha Nigos <ishanigos@Roeshas-MacBook-Pro.local>
2018-09-16 09:06:27 -0500

    emotion dictionary

--- vidUpload.py
+++ vidUpload.py
@@ -1,34 +1,41 @@
 import http.client, urllib.request, urllib.parse, urllib.error, base64
 import json
 
-<<<<<<< HEAD
+
 import cognitive_face as CF
 import cv2
 import os
 
 
-key = '9f23d33e86a24404b089d5a02ed69090'
-CF.Key.set(key)
+# Sets our azure subscription key in the headers
+headers1 = {
+    'Ocp-Apim-Subscription-Key': 'e74bfd759b104d9b8503b05c0f66af1f'
+}
 
+# def extractstt(data): # data a string of the json dictionary
+#     print("Extractstt start")
+#     part = json.loads(data)
+#     print(data)
+#     print("Extractstt end")
+#     pass
+#
+
+
+key = 'e74bfd759b104d9b8503b05c0f66af1f'
+CF.Key.set(key)
 #prints the key
 print(key)
+
+base_url = 'https://westus2.api.cognitive.microsoft.com/face/v1.0'
+CF.BaseUrl.set(base_url)
 
 accountID = "e6dc5dac-de07-48a6-b157-91105accc78a"
 accAccessToken = ""
 
-# Sets our azure subscription key in the headers
-headers1 = {
-    'Ocp-Apim-Subscription-Key': '8dbcfd113c304bb4a6a31b8dcb715385'
-}
 
-def extractstt(data): # data a string of the json dictionary
-    print("Extractstt start")
-
-
-    part = json.loads(data)
-    print(data)
 
 results = []
+
 # need to convert video to frames
 # frame to .jpg format (if this isn't done already)
 # vidcap = cv2.VideoCapture('emotion.mp4')
@@ -42,10 +49,6 @@ results = []
 #   count += 1
 
 
-# face.detect method can take image files of .jpg, .png, or .bmp
-
-    print("Extractstt end")
-    pass
 
 
 path, dirs, files = next(os.walk("frames"))
@@ -59,21 +62,21 @@ for i in range(file_count):
 
 print(results)
 
-
-try:
-    conn = http.client.HTTPSConnection('api.videoindexer.ai')
-    conn.request("GET", "/auth/westus2/Accounts/e6dc5dac-de07-48a6-b157-91105accc78a/AccessToken?%s" % params, "{body}",
-                 headers1)
-    response = conn.getresponse()
-    string = response.read().decode('utf-8') # want function to extract data from string
-    speechtotext = extractstt(string)
-    # json_obj = json.loads(string) # literally all our data is in string, json_obj loads prettily, supposedly
-    # print(json_obj)
-    # accAccessToken = json_obj
-    # print data is the access token
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+#
+# try:
+#     conn = http.client.HTTPSConnection('api.videoindexer.ai')
+#     conn.request("GET", "/auth/westus2/Accounts/e6dc5dac-de07-48a6-b157-91105accc78a/AccessToken?%s" % params, "{body}",
+#                  headers1)
+#     response = conn.getresponse()
+#     string = response.read().decode('utf-8') # want function to extract data from string
+#     speechtotext = extractstt(string)
+#     # json_obj = json.loads(string) # literally all our data is in string, json_obj loads prettily, supposedly
+#     # print(json_obj)
+#     # accAccessToken = json_obj
+#     # print data is the access token
+#     conn.close()
+# except Exception as e:
+#     print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
 # step 2 video access token using part 1
 
@@ -84,58 +87,58 @@ except Exception as e:
 
 # https://github.com/Microsoft/Cognitive-Face-Python/blob/master/cognitive_face/face.py
 
-try:
-    conn = http.client.HTTPSConnection('api.videoindexer.ai')
-    conn.request("GET",
-                 "/auth/westus2/Accounts/e6dc5dac-de07-48a6-b157-91105accc78a/Videos/b32f841588/AccessToken?%s" % params,
-                 "{body}", headers2)
-    response = conn.getresponse()
-    string = response.read().decode('utf-8')
-    json_obj = json.loads(string)
-    # print(json_obj)
-    videoAccessToken = json_obj
-    # print data is the access token
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
-
-# step 3
-headers3 = {
-    # Request headers
-    'Ocp-Apim-Subscription-Key': '8dbcfd113c304bb4a6a31b8dcb715385',
-}
-
-params = urllib.parse.urlencode({
-    # Request parameters
-    'accessToken': accAccessToken,
-    'language': 'English',
-})
-
-
-def parser(data):
-    '''
-    data is json obj
-    '''
-    data = json.loads(data)
-    # data seems to be able to be keyed into twice as dictionary, then holds lists
-    # data['summarizedInsights']['faces'] is a list of more dictionaries that hold all of the face information (id, appearances, etc)
-    print(data['summarizedInsights']['faces'])
-
-
-try:
-    conn = http.client.HTTPSConnection('api.videoindexer.ai')
-    conn.request("GET", "/westus2/Accounts/e6dc5dac-de07-48a6-b157-91105accc78a/Videos/b32f841588/Index?%s" % params,
-                 "{body}", headers3)
-    response = conn.getresponse()
-    data = response.read()
-    # thing that needs to be parsed 
-    print('\n', data, '\n')
-    # other possibility for parsing
-    # data = str(data)
-    parser(data)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+# try:
+#     conn = http.client.HTTPSConnection('api.videoindexer.ai')
+#     conn.request("GET",
+#                  "/auth/westus2/Accounts/e6dc5dac-de07-48a6-b157-91105accc78a/Videos/b32f841588/AccessToken?%s" % params,
+#                  "{body}", headers2)
+#     response = conn.getresponse()
+#     string = response.read().decode('utf-8')
+#     json_obj = json.loads(string)
+#     # print(json_obj)
+#     videoAccessToken = json_obj
+#     # print data is the access token
+#     conn.close()
+# except Exception as e:
+#     print("[Errno {0}] {1}".format(e.errno, e.strerror))
+#
+# # step 3
+# headers3 = {
+#     # Request headers
+#     'Ocp-Apim-Subscription-Key': '8dbcfd113c304bb4a6a31b8dcb715385',
+# }
+#
+# params = urllib.parse.urlencode({
+#     # Request parameters
+#     'accessToken': accAccessToken,
+#     'language': 'English',
+# })
+#
+#
+# def parser(data):
+#     '''
+#     data is json obj
+#     '''
+#     data = json.loads(data)
+#     # data seems to be able to be keyed into twice as dictionary, then holds lists
+#     # data['summarizedInsights']['faces'] is a list of more dictionaries that hold all of the face information (id, appearances, etc)
+#     print(data['summarizedInsights']['faces'])
+#
+#
+# try:
+#     conn = http.client.HTTPSConnection('api.videoindexer.ai')
+#     conn.request("GET", "/westus2/Accounts/e6dc5dac-de07-48a6-b157-91105accc78a/Videos/b32f841588/Index?%s" % params,
+#                  "{body}", headers3)
+#     response = conn.getresponse()
+#     data = response.read()
+#     # thing that needs to be parsed
+#     print('\n', data, '\n')
+#     # other possibility for parsing
+#     # data = str(data)
+#     parser(data)
+#     conn.close()
+# except Exception as e:
+#     print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
 
 
